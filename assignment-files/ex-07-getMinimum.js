f37507934ae7508dd9825fd937f4640afff293d5
Yair <hello@agzeri.com>
2018-07-15 21:18:54 -0500

    Homework JS Basics

---
+++ assignment-files/ex-07-getMinimum.js
@@ -0,0 +1,32 @@
+/**
+ * getMinimum()
+ * ------------------------------------------------------
+ * Write a function called `getMinimum()` that takes 4 numbers as arguments
+ * and returns the one with the lowest value.
+ *
+ **/
+
+function getMinimum(num1, num2, num3, num4){
+}
+
+
+// *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*
+// *~*~*~*~*~*~*~* Tests (Don't Touch) *~*~*~*~*~*~*~*~*
+// *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*
+
+
+/*-------------------TEST-1-------------------------*/
+// function receives 4 number inputs
+//    and returns the minimum value
+/*--------------------------------------------------*/
+console.log("==== ex-07-getMinimum : TEST 1 ====");
+
+
+console.assert( getMinimum(1,2,4,0) === 0 )
+console.assert( getMinimum(1,2,4,0) !== 1 )
+
+console.assert( getMinimum(4,8,4,20) === 4 )
+
+console.assert( getMinimum(300,-2,-99,0) === -99 )
+
+console.assert( getMinimum(-5,-11,4,-5) === -11 )
